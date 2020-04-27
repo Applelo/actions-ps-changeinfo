e8aa9ede80d7ff629841697a21ef21c961f3bf41
Loïs Boubault <lois@principal.studio>
2020-04-27 18:51:15 -0400

    Remove old test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,5 @@
 import {create} from '../src/create';
 import {parse} from '../src/parse';
-import * as process from 'process';
-import * as cp from 'child_process';
-import * as path from 'path';
 import fs from 'fs';
 
 function readAFile(path: string): Promise<string> {
@@ -27,14 +24,4 @@ test('changeinfo', async () => {
   const changeinfo = await readAFile('__tests__/changeinfo.xml');
   const expected = await readAFile('__tests__/expected.xml');
   expect(expected).toMatch(expected);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500';
-  const ip = path.join(__dirname, '..', 'lib', 'main.js');
-  const options: cp.ExecSyncOptions = {
-    env: process.env,
-  };
-  console.log(cp.execSync(`node ${ip}`, options).toString());
 });
