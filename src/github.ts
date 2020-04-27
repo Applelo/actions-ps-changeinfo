a1b3102ec9f899ca354097050c0ac905323851f0
Loïs Boubault <lois@principal.studio>
2020-04-27 19:00:33 -0400

    change import in github

--- src/github.ts
+++ src/github.ts
@@ -1,16 +1,19 @@
-import github from '@actions/github';
+import {GitHub, context} from '@actions/github';
 
 export async function gitHub(token: string): Promise<boolean> {
   return new Promise(async resolve => {
-    const octokit = new github.GitHub(token);
-    const context = github.context;
+    const octokit = new GitHub(token);
 
-    await octokit.pulls.create({
-      ...context.repo,
-      title: '[Vita Changeinfo] New changeinfo update',
-      head: `${context.repo.owner}:vita-changeinfo`,
-      base: 'master',
-    });
+    await octokit.pulls
+      .create({
+        ...context.repo,
+        title: '[Vita Changeinfo] New changeinfo update',
+        head: `${context.repo.owner}:vita-changeinfo`,
+        base: 'master',
+      })
+      .catch(error => {
+        throw error;
+      });
 
     resolve(true);
   });
